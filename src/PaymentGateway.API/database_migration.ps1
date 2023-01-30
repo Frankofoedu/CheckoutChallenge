1a63d0404295ba722b2b0d44ed54004d2406bf4a
Frank Ofoedu <frankofoedu@gmail.com>
2023-01-30 21:24:15 +0100

    add database migration script

---
+++ src/PaymentGateway.API/database_migration.ps1
@@ -0,0 +1,29 @@
+
+$ErrorActionPreference = 'Stop'
+
+try {
+    Set-PSDebug -Trace 1 # log commands before running https://stackoverflow.com/a/49647445
+
+    
+    $filepath = Join-Path $PSScriptRoot "appsettings.UnitTests.json"
+    $appSettings = (Get-Content $filepath | Out-String | ConvertFrom-Json)
+
+    $postgresConnection = $appSettings.ConnectionStrings.PostgresConnection
+
+    $output = .\efbundle.exe --connection $postgresConnection --verbose    #https://stackoverflow.com/a/61674353
+
+    if ($LastExitCode -ne 0) { #catch efcore errors and log them
+    
+        Write-Output $output
+        exit(1)
+    }
+}
+Catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Output $ErrorMessage
+
+    Write-Output $output
+    exit(1)
+}
+Write-Output $output
+
